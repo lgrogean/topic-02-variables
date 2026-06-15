61460e4e6aab97448288f8c0b80dbdc21c151dcb
lgrogean <lgrogean2@edisonohio.edu>
2026-06-14 22:54:27 -0400

    Displayed personal data using variables and printed results

---
+++ solution.py
@@ -0,0 +1,29 @@
+# Topic 02 Collaborative Assignment
+# Your Name: Lauren Grogean
+# Date: 06/14/26
+
+# --- STARTER CODE ---
+# Step 1: Store some information about yourself
+name = Lauren          # Replace with your name
+favorite_number = 7  # Replace with your favorite number
+
+# Step 2: Write an expression using your variables
+# Example: double = favorite_number * 2
+
+# Step 3: Print a summary
+# Example: print("My name is", name)
+
+# --- YOUR CODE BELOW THIS LINE ---
+name = Lauren
+age = 20
+sibling_number = 7
+favorite_number = 7
+favorite_number = 2**3 - 1
+favorite_color = purple
+
+print("My name is", name)
+print("My age is", age)
+print("My sibling number is", sibling_number)
+print("My favorite color is", favorite_color)
+print("My favorite number is," favorite_number)
+print("Equation;", "2**3 - 1 =", 2**3 - 1)
